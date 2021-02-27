5da244a44ec38b9169fc915511d7fd7dd69ac254
Kevin Sousa De Almeida <kevin@Hell.Kevin>
2021-02-27 11:53:44 -0300

    add link model

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const chromeWebdriver = require('chromedriver');
 
+// -----------------------------------------------------------//
+//imports models
+
+const link = require('./models/link');
 
 (async function Start(){
 
     chrome.setDefaultService(new chrome.ServiceBuilder(chromeWebdriver.path).build())
     let driver = await new webdriver.Builder().forBrowser('chrome').build();
-        driver.get('https://www.google.com');
+        driver.get(link.url);
         console.log('yes, its work, commit this shit')
     try{
     }catch(error){
---
+++ src/models/link.js
@@ -0,0 +1,5 @@
+const link = {
+    url: 'https://web.whatsapp.com/'
+}
+
+module.exports = link;
